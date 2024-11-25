6a330d13fcd8d02699d76829ea546f3910dd9f60
Gurudev Devkar <169146749+guru6910@users.noreply.github.com>
2024-11-25 17:47:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-    region = "ap-northeast-1"
-  
+  region = "ap-northeast-1"
 }
-resource "aws_instance" "instance" {
-    ami = "ami-0b20f552f63953f0e"
-    instance_type = "t2.micro"
-    availability_zone = "ap-northeast-1a"
 
-    tags = {
-        name = "terraform_instace"
-    }
+resource "aws_instance" "instance" {
+  ami           = "ami-0b20f552f63953f0e"
+  instance_type = "t2.micro"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "terraform_instance"
+  }
 }
